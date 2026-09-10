e8ff3378ba56d8a623b01fc44e7fcf4dcd829bbe
Le Duc Tho <leductho911@gmail.com>
2026-09-10 17:36:34 +0700

    feat: implement Lyra carousel navigation and update clangd configuration

--- src/activities/home/HomeActivity.h
+++ src/activities/home/HomeActivity.h
@@ -12,6 +12,8 @@ struct Rect;
 class HomeActivity final : public Activity {
   ButtonNavigator buttonNavigator;
   int selectorIndex = 0;
+  int carouselBookIndex = 0;
+  int carouselMenuIndex = 0;
   bool recentsLoading = false;
   bool recentsLoaded = false;
   bool firstRenderDone = false;
